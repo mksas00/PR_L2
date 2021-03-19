ec0670b639d6ed7eee22273393c71d31b8547a84
mikolaj.sas <beqs171@gmail.com>
2021-03-19 21:19:26 +0100

    lab nr 3

---
+++ navbar.js
@@ -0,0 +1,3 @@
+let nav = '<nav><a href="index.html">Home</a>' + '<nav><a href="contact.html">Kontakt</a> </nav>';
+let navbarElem = document.querySelector(".navbar"); //lub inny sposób opisany wyżej
+navbarElem.innerHTML = nav;
